7a0a53cbb2955a2c9e425279a0a21524b71644df
Joe Bentley <joe.bentley@qt.io>
2025-06-25 16:06:30 +0200

    Add start and stop buttons to trigger the every 30 min stand notif.

--- Standing Desk/AppDelegate.swift
+++ Standing Desk/AppDelegate.swift
@@ -10,9 +10,15 @@
 import Cocoa
 import UserNotifications
 
+let setupNotificationIdentifier = "com.joebentley.setupstandnotification"
+let repeatingNotificationIdentifier = "com.joebentley.standnotification"
+
 class AppDelegate: NSObject, NSApplicationDelegate {
     private var statusItem: NSStatusItem!
     private var notificationCenter: UNUserNotificationCenter!
+    
+    private var startButton: NSMenuItem!
+    private var stopButton: NSMenuItem!
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         statusItem = NSStatusBar.system.statusItem(
@@ -30,7 +36,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             do {
                 let granted = try await notificationCenter.requestAuthorization(
                     options: [.alert, .badge])
-                if !granted {
+                if granted {
+                    // Cancel the in-flight notification
+                    _cancel_repeating_notification()
+                } else {
                     let alert = NSAlert()
                     alert.messageText =
                         "This app needs notifications to be useful!"
@@ -43,15 +52,30 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
         setupMenus()
     }
+    
+    func applicationWillTerminate(_ notification: Notification) {
+        _cancel_repeating_notification()
+    }
 
     func setupMenus() {
         let menu = NSMenu()
-        let test = NSMenuItem(
-            title: "Test",
-            action: #selector(didTapTest),
+        menu.autoenablesItems = false
+        
+        startButton = NSMenuItem(
+            title: "Start timer",
+            action: #selector(didClickStart),
             keyEquivalent: "t"
         )
-        menu.addItem(test)
+        menu.addItem(startButton)
+        
+        stopButton = NSMenuItem(
+            title: "Stop timer",
+            action: #selector(didClickStop),
+            keyEquivalent: "s"
+        )
+        stopButton.isEnabled = false
+        menu.addItem(stopButton)
+        
         menu.addItem(NSMenuItem.separator())
         menu.addItem(
             NSMenuItem(
@@ -62,16 +86,50 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         )
         statusItem.menu = menu
     }
-
-    @objc func didTapTest() {
+    
+    func _cancel_repeating_notification() {
+        notificationCenter.removePendingNotificationRequests(withIdentifiers: [repeatingNotificationIdentifier])
+    }
+    
+    func _post_start_notification() {
         let content = UNMutableNotificationContent()
-        content.title = "Stand reminder!"
-        content.body = "Don't forget to stand!"
+        content.title = "Notification setup"
+        content.body = "You will be notified every 30 minutes"
+        
         let notification = UNNotificationRequest(
-            identifier: "com.joebentley.standnotification",
+            identifier: setupNotificationIdentifier,
             content: content,
             trigger: nil
         )
         notificationCenter.add(notification)
+    }
+    
+    func _post_repeating_notification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Stand reminder"
+        content.body = "Don't forget to stand!"
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: (30*60), repeats: true)
+        
+        let notification = UNNotificationRequest(
+            identifier: repeatingNotificationIdentifier,
+            content: content,
+            trigger: trigger
+        )
+        notificationCenter.add(notification)
+    }
+
+    @objc func didClickStart() {
+        _post_start_notification()
+        _post_repeating_notification()
+        
+        startButton.isEnabled = false
+        stopButton.isEnabled = true
+    }
+    
+    @objc func didClickStop() {
+        _cancel_repeating_notification()
+        startButton.isEnabled = true
+        stopButton.isEnabled = false
     }
 }
